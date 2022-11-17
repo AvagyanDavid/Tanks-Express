4d556e59458ec7ec301e5ba20956f6de5e276ab6
AvagyanDavid <avagyan.d.i.21@gmail.com>
2022-11-17 12:07:36 +0300

    add 3 page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'World of Tanks' });
 });
+
+router.get('/heavy_tank', function(req, res, next) {
+  res.send("<h1>Страница Тяжелые танки </h1>");
+});
+
+router.get('/average_tank', function(req, res, next) {
+  res.send("<h1>Страница Средние танки </h1>");
+});
+
+router.get('/pt', function(req, res, next) {
+  res.send("<h1>Страница ПТ </h1>");
+});
+
 
 module.exports = router;
