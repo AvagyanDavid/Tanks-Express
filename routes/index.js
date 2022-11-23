e281394d35b7c7d9c88132455b060d54e89349f9
AvagyanDavid <avagyan.d.i.21@gmail.com>
2022-11-23 22:57:03 +0300

    add link

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'World of Tanks' });
+  res.render('index', { title: 'World of Tanks',
+  desc: "Добро пожаловать в гайд по классам техники, здесь будет описано какие роли у классов и что от них требует команда"});
 });
 
 /* Тяжелые танки */
