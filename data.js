b81be5001e5ad94481684592a1552918875eb22d
AvagyanDavid <avagyan.d.i.21@gmail.com>
2022-11-29 08:42:47 +0300

    add values to db

---
+++ data.js
@@ -0,0 +1,22 @@
+var data = [{
+title: 'ТТ',
+nick: 'heavy_tank',
+avatar: '/images/ИС_3.jpg',
+desc: 'Самый бронированный класс техники.'
+},
+{
+title: 'СТ',
+nick: 'average_tank',
+avatar: '/images/Т-34-85.jpg',
+desc: 'Многофункциональный класс техники'
+},
+{
+title: 'ПТ',
+nick: 'PT',
+avatar: '/images/Grille.jpg',
+desc: 'Орудие у данного класса техники самое пробивное.'
+}
+];
+
+
+module.exports.data = data;
