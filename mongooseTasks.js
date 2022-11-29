19da3c5fbec5200d927c6ff55f4eec357c3134e5
AvagyanDavid <avagyan.d.i.21@gmail.com>
2022-11-29 23:27:15 +0300

    upgreate mongooseTasks.js

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1/test')
 
-var Cat = mongoose.model('Cat', { name: String })
+var Tanks = mongoose.model('Tanks', { name: String })
 
-var kitty = new Cat({ name: 'Пушок' })
-kitty.save(function (err) {
+var Tanks = new Tanks({ name: 'Heavy_tanks' })
+Tanks.save(function (err) {
     if (err) {
         console.log(err)
     } else {
-        console.log('Мяу')
+        console.log('Брониованный класс техники')
     }
 })
