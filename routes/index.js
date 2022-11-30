35901cb714d72e94fa9e9d502fc56f06afdc1c9b
AvagyanDavid <avagyan.d.i.21@gmail.com>
2022-11-30 22:44:39 +0300

    clear code

--- routes/index.js
+++ routes/index.js
@@ -7,31 +7,4 @@ router.get('/', function(req, res, next) {
   desc: "Добро пожаловать в гайд по классам техники, здесь будет описано какие роли у классов и что от них требует команда"});
 });
 
-/* Тяжелые танки */
-router.get('/heavy_tank', function(req, res, next) {
-  res.render('tank', {
-    title: "Тяжелые танки",
-    picture: "images/ИС_3.jpg",
-    desc: "Самый бронированный класс техники"
-  });
-});
-
-/* Средние танки */
-router.get('/average_tank', function(req, res, next) {
-  res.render('tank', {
-    title: "Средние танки",
-    picture: "images/Т-34-85.jpg",
-    desc: "Многофункциональный класс техники"
-  });
-});
-
-/* ПТ */
-router.get('/pt', function(req, res, next) {
-  res.render('tank', {
-    title: "ПТ",
-    picture: "images/Grille.jpg",
-    desc: "Орудие у данного класса техники самое пробивное"
-  });
-});
-
 module.exports = router;
