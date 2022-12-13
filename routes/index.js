7994a930c19ad83f66a5df4921546768f28bc391
AvagyanDavid <avagyan.d.i.21@gmail.com>
2022-12-13 23:49:59 +0300

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,7 @@ var Tank = require("../models/tanks/tank").Tank
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Tank.find({},{_id:0,title:1,nick:1},function(err,menu){
-    res.render('index', {
-      title: 'Express',
-      menu: menu
-    });
-  })
+  res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express', menu:menu });
 });
 
 module.exports = router;
