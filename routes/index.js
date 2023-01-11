7f9c818f7386617fe6980f99fa4826284cee7c11
AvagyanDavid <avagyan.d.i.21@gmail.com>
2023-01-12 00:16:49 +0300

    fix bugs

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ router.get('/', function(req, res, next) {
   Tank.find({},{_id:0,title:1,nick:1},function(err,menu){
     res.render('index', {
       title: 'Express',
-      menu: menu
+      menu: menu,
+      desc: "Добро пожаловать в гайд по классам техники, здесь будет описано какие роли у классов и что от них требует команда"
     });
   })
 });
--- routes/tanks.js
+++ routes/tanks.js
@@ -11,19 +11,20 @@ router.get('/', function(req, res, next) {
 /* Страница классов */
 router.get("/:nick", function(req, res, next){
 async.parallel([
-        function(callback){
-            Tank.findOne({nick:req.params.nick},callback)
-        },
-        function(callback){
-            Tank.find({},{_id:0,title:1,nick:1},
-            callback)
-        }
-    ],
+    function(callback){
+        Tank.findOne({nick:req.params.nick},callback)
+    },
+    function(callback){
+        Tank.find({},{_id:0,title:1,nick:1},
+        callback)
+    }
+],
     function(err,result){
         if(err) return next(err)
         var tank = result[0]
         var tanks = result[1] || []
         if(!tank) return next(new Error("Нет такого класса в игре"))
+        console.log(tank.avatar)
         res.render('tank', {
             title: tank.title,
             picture: tank.avatar,
