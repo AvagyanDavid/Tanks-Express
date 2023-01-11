4e623d1ee081210ba7a8ae4fde7bff924b2c291e
AvagyanDavid <avagyan.d.i.21@gmail.com>
2023-01-11 23:02:07 +0300

    create sessions

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var Tank = require("../models/tanks/tank").Tank
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express', menu:menu });
+  req.session.greeting = "Hi!!!"
+  res.render('index', { title: 'Express', menu:menu });
 });
 
 module.exports = router;
