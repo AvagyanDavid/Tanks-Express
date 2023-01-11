8ebf7d6f617e28b1c20d2fdcbaa465b06ae5ce82
AvagyanDavid <avagyan.d.i.21@gmail.com>
2023-01-11 23:55:04 +0300

    create counter

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,8 @@ var router = express.Router();
 var Tank = require("../models/tanks/tank").Tank
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  req.session.greeting = "Hi!!!"
-  res.render('index', { title: 'Express', menu:menu });
+router.get("/", function(req, res, next) {
+  res.render('index', { title: 'Express', menu:menu, counter:req.session.counter });
 });
 
 module.exports = router;
