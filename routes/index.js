ecf01d67aa9e80ea9738d1a0b4378fa5d7dd9bfc
AvagyanDavid <avagyan.d.i.21@gmail.com>
2023-01-12 18:01:04 +0300

    add cookie

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var Tank = require("../models/tanks/tank").Tank
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Tank.find({},{_id:0,title:1,nick:1},function(err,menu){
-    res.render('index', {
+    res.cookie('greeting', 'Hi!!!').render('index', {
       title: 'Express',
       menu: menu,
       desc: "Добро пожаловать в гайд по классам техники, здесь будет описано какие роли у классов и что от них требует команда"
