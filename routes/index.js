0b7cc2544eda1ec7e64ab774165d8c8631942417
AvagyanDavid <avagyan.d.i.21@gmail.com>
2023-01-12 19:45:05 +0300

    save session on mongo

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var Tank = require("../models/tanks/tank").Tank
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Tank.find({},{_id:0,title:1,nick:1},function(err,menu){
-    res.cookie('greeting', 'Hi!!!').render('index', {
+    req.session.greeting = "Hi!!!"
+    res.render('index', {
       title: 'Express',
       menu: menu,
       desc: "Добро пожаловать в гайд по классам техники, здесь будет описано какие роли у классов и что от них требует команда"
